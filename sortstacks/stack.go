581ac06dec49c3350b8677035577d7aa37c12a95
Vincent BASTIONI <v.bastioni@gmail.com>
2020-03-20 17:21:21 +0100

    wololo

--- sortstacks/stack.go
+++ sortstacks/stack.go
@@ -8,29 +8,29 @@ import (
 
 type sortStacks struct {
 	a, b []int
-	ops []string
-	f map[string]func()
+	ops  []string
+	f    map[string]func()
 }
 
 type SortStacks interface {
-	helperNew()
+	// helperNew()
 	SetOps([]string)
 	CheckOps()
 	GetFunc(op string) func()
 	Play()
 	IsOk() bool
 
-/*	SwapA()
-	SwapB()
-	SwapBoth()
-	PushA()
-	PushB()
-	RotateA()
-	RotateB()
-	RotateBoth()
-	RevRotateA()
-	RevRotateB()
-	RevRotateBoth()*/
+	/*	SwapA()
+		SwapB()
+		SwapBoth()
+		PushA()
+		PushB()
+		RotateA()
+		RotateB()
+		RotateBoth()
+		RevRotateA()
+		RevRotateB()
+		RevRotateBoth()*/
 }
 
 func (st *sortStacks) CheckOps() {
@@ -55,39 +55,39 @@ func (st *sortStacks) GetOps() []string {
 
 func (st *sortStacks) helperNew() {
 	st.f = map[string]func(){
-		"sa": st.SwapA,
-		"sb": st.SwapB,
-		"ss": st.SwapBoth,
-		"pa": st.PushA,
-		"pb": st.PushB,
-		"ra": st.RotateA,
-		"rb": st.RotateB,
-		"rr": st.RotateBoth,
+		"sa":  st.SwapA,
+		"sb":  st.SwapB,
+		"ss":  st.SwapBoth,
+		"pa":  st.PushA,
+		"pb":  st.PushB,
+		"ra":  st.RotateA,
+		"rb":  st.RotateB,
+		"rr":  st.RotateBoth,
 		"rra": st.RevRotateA,
 		"rrb": st.RevRotateB,
 		"rrr": st.RevRotateBoth,
 	}
 }
 
-func New(i []int) (st SortStacks) {
-	st = &sortStacks{
+func New(i []int) SortStacks {
+	st := &sortStacks{
 		a: i,
 		b: []int{},
 	}
 	st.helperNew()
-	return
+	return st
 }
 
 func (st *sortStacks) Play() {
 	for _, o := range st.ops {
-		fmt.Println(st.a, st.b)// VERBOSE
+		fmt.Println(st.a, st.b) // VERBOSE
 		st.f[o]()
 	}
-	fmt.Println(st.a, st.b)// VERBOSE
+	fmt.Println(st.a, st.b) // VERBOSE
 }
 
 func (st *sortStacks) IsOk() bool {
-	if len(st.b) > 0 || sort.IntsAreSorted(st.a) ==false {
+	if len(st.b) > 0 || sort.IntsAreSorted(st.a) == false {
 		return false
 	} else {
 		return true
